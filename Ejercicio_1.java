b9082637297c60befc8071fe759faa7d81f0360a
ProfeAndIbito <andres.ibito1989@gmail.com>
2025-10-14 18:11:33 -0500

    Add files via upload

---
+++ Ejercicio_1.java
@@ -0,0 +1,26 @@
+
+package secuenciales;
+
+/**
+ *
+ * @author Andrés Ibito
+ */
+public class Ejercicio_1 {
+
+    public static void main(String[] args) {
+     
+        //Crear variables
+        String username="Camila";
+        int cofreUno=8;
+        int cofreDos=12;
+        int sumaDulces;
+        
+        //Proceso
+        sumaDulces=cofreUno + cofreDos;
+        
+        //Salida
+        System.out.print(username + " ahora tiene un total de " + sumaDulces + " caramelos mágicos.");
+        
+    }
+    
+}
